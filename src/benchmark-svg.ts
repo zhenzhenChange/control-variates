7abc7706addf4d5436de148e61dbff729ce1231f
zhongzhen <1506678296@qq.com>
2023-04-18 15:29:34 +0800

    ⚗️ render os

--- src/benchmark-svg.ts
+++ src/benchmark-svg.ts
@@ -1,12 +1,29 @@
 import { join } from 'node:path'
+import { platform } from 'node:os'
 import { readFileSync } from 'node:fs'
 
 import { BenchmarkResult } from './benchmark-shared'
 
+const getOS = () => {
+  switch (platform()) {
+    case 'linux':
+      return 'Linux'
+
+    case 'win32':
+      return 'Windows'
+
+    case 'darwin':
+      return 'MacOS'
+
+    default:
+      return `Unknown OS: ${platform()}`
+  }
+}
+
 // TODO Optimize
 export function createSVGTemplate(documentTitle: string, benchmarkResults: BenchmarkResult[]) {
   return readFileSync(join(process.cwd(), 'SVG-Template.html'), { encoding: 'utf8' })
-    .replace(/\{\{\s+documentTitle\s+\}\}/gi, documentTitle)
+    .replace(/\{\{\s+documentTitle\s+\}\}/gi, `${documentTitle}-${getOS()}`)
     .replace(/\{\{\s+benchmarkYAxis\s+\}\}/gi, JSON.stringify(benchmarkResults[0].records.map((record) => record.vars)))
     .replace(/\{\{\s+benchmarkResults\s+\}\}/gi, JSON.stringify(benchmarkResults))
 }
